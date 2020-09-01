e428f6d321fe58c8c171a4337dde7e2ce7eebbd9
Felix Angelov <felangelov@gmail.com>
2020-09-01 16:53:12 -0500

    chore: v1.0.1 (#7)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,8 @@
+# 1.0.1
+
+- docs: README improvements
+- docs: inline documentation improvements
+
 # 1.0.0
 
 - Add `analysis_options` used at [Very Good Ventures](https://verygood.ventures)
--- lib/very_good_analysis.dart
+++ lib/very_good_analysis.dart
@@ -1,3 +1,13 @@
+/// Very Good Dart analyzer settings and best practices
+/// used internally at [Very Good Ventures](https://verygood.ventures).
 library very_good_analysis;
 
+/// Exported from [package:pedantic](https://pub.dev/packages/pedantic)
+/// Indicates to tools that `Future` is intentionally not `await`-ed.
+///
+/// In an `async` context, it is normally expected that all `Future`s are
+/// awaited, and that is the basis of the lint `unawaited_futures`. However,
+/// there are times where one or more futures are intentionally not awaited.
+/// This function may be used to ignore a particular future. It silences the
+/// `unawaited_futures` lint.
 export 'package:pedantic/pedantic.dart' show unawaited;
