545c1794188f87fee51fb30e0e9434e7103735eb
Felix Angelov <felangelov@gmail.com>
2022-09-14 16:35:49 -0500

    chore: v3.1.0

--- lib/very_good_analysis.dart
+++ lib/very_good_analysis.dart
@@ -1,13 +1,3 @@
 /// Very Good Dart analyzer settings and best practices
 /// used internally at [Very Good Ventures](https://verygood.ventures).
 library very_good_analysis;
-
-/// Indicates to tools that `Future` is intentionally not `await`-ed.
-///
-/// In an `async` context, it is normally expected that all `Future`s are
-/// awaited, and that is the basis of the lint `unawaited_futures`. However,
-/// there are times where one or more futures are intentionally not awaited.
-/// This function may be used to ignore a particular future. It silences the
-/// `unawaited_futures` lint.
-@Deprecated('Please use unawaited from dart:async.')
-void unawaited(Future<void>? future) {}
