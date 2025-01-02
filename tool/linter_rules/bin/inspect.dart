b112be4d070ecf0f3ee1357bb3269256e10badda
Alejandro Santiago <me@alestiago.com>
2025-01-02 12:39:09 +0000

    feat: define inspect script

---
+++ tool/linter_rules/bin/inspect.dart
@@ -0,0 +1,44 @@
+import 'dart:convert';
+
+import 'package:http/http.dart';
+import 'package:linter_rules/linter_rules.dart';
+import 'package:linter_rules/src/models/linter_rule.dart';
+
+/// The [Uri] to fetch all linter rules from.
+final _allLinterRulesUri = Uri.parse(
+  'https://raw.githubusercontent.com/dart-lang/site-www/refs/heads/main/src/_data/linter_rules.json',
+);
+
+Future<void> main({
+  void Function(String) log = print,
+}) async {
+  const version = '7.0.0';
+
+  final response = await get(_allLinterRulesUri);
+  final json = jsonDecode(response.body) as List<dynamic>;
+
+  final dartRules = json
+      .map(
+        (rule) => LinterRule.fromJson(rule as Map<String, dynamic>),
+      )
+      .toList();
+  log('Fetched ${dartRules.length} Dart linter rules');
+
+  final vgaRules = await allVeryGoodAnalysisRules(version: version);
+  log('Fetched ${vgaRules.length} Very Good Analysis rules');
+  log('');
+
+  final deprecatedDartRules = dartRules
+      .where((rule) => rule.state == LinterRuleState.deprecated)
+      .map((rule) => rule.name)
+      .toSet();
+  final deprecatedVgaRules =
+      vgaRules.where(deprecatedDartRules.contains).toList();
+  final deprecationMessage = StringBuffer(
+    '''Found ${deprecatedVgaRules.length} deprecated Dart rules (out of ${deprecatedDartRules.length} deprecated rules) in Very Good Analysis ($version)${deprecatedVgaRules.isEmpty ? '.' : ':'}''',
+  );
+  for (final rule in deprecatedVgaRules) {
+    deprecationMessage.write('\n  - $rule');
+  }
+  log(deprecationMessage.toString());
+}
